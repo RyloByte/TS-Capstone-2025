880152d5ea03a80680c55b7f6043fe4a1bef0586
Josh Bedwell <rcxwhiz@gmail.com>
2025-02-18 14:31:29 -0800

    ignored data and result directories

--- hyperpackage_snakemake/workflow/scripts/id_mapping.py
+++ hyperpackage_snakemake/workflow/scripts/id_mapping.py
@@ -1,5 +1,16 @@
 from Bio import SeqIO
 from snakemake.script import snakemake
+import re
+
+
+id_patterns = {
+    "Uniprot/Swiss-Prot": re.compile(r"[OPQ]\d[A-Z0-9]{3}\d|[A-NR-Z]\d([A-Z][A-Z0-9]{2}\d){1,2}"),
+    "NCBI/RefSeq": re.compile(r"[A-Z]{2}_[A-Z0-9]+\.\d+"),
+    "GenBank": re.compile(r"[A-Z]{2}\d{5}\d*"),
+    "PDB": "",
+    "TrEMBLE": ""
+}
+
 
 
 # snakemake.input[0] = "data/{sample}.faa"
