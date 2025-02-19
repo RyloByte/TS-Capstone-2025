daea26e5725cb8194eaa46c63eac7d80b1e5b0c9
Thomas <janast@berkeley.edu>
2025-02-18 23:34:07 -0600

    extracted tar.gz files, create input output and temp folder, and run mmseqs. All we need is to split the files by cluster and then condense them into tar.gz files 🥹

--- hyperpackage_snakemake/workflow/scripts/sequence_clustering.py
+++ hyperpackage_snakemake/workflow/scripts/sequence_clustering.py
@@ -1,13 +1,46 @@
 from snakemake.script import snakemake
 from Bio import SeqIO
 import os
+import tarfile
+import subprocess
 
 
 CLUSTER_THRESHOLD = snakemake.config["sequence_cluster_threshold"]
 
+def extract_tar_gz(archive_path, extract_to="./data/inputs"):
+    with tarfile.open(archive_path, "r:gz") as tar:
+        os.makedirs(extract_to, exist_ok=True)
+        tar.extractall(path=extract_to)
 
-# snakemake.input[0] = "data/{sample}-structure_clusters.tar.gz", fill with {0..n}.faa
+# snakemake.input[0] = "data/{sample}-structure_clusters.tar.gz" , fill with {0..n}.faa
 # snakemake.output[0] = "data/{sample}-sequence_clusters.tar.gz", fill with {0..n}.faa
 if __name__ == "__main__":
-    for file in os.listdir(snakemake.input[0]):
-        sequences = SeqIO.parse(os.path.join(snakemake.input[0], file), "fasta")
+    extract_path = "./data/inputs"
+    extract_tar_gz(snakemake.input[0], extract_path)
+
+    output_path = "./data/outputs"
+    os.makedirs(output_path, exist_ok=True)
+
+    tmp_dir = "./data/tmp"
+    os.makedirs(tmp_dir, exist_ok=True)
+
+    for file in os.listdir(extract_path):
+        if file.startswith("._"):
+            continue
+        filepath = os.path.join(extract_path, file)
+        filename = file.rsplit("-", 1)[0]
+        subprocess.run(["mmseqs", "easy-linclust", os.path.join(extract_path, file), f"{output_path}/{filename}-sequence_clusters.faa", tmp_dir])
+
+    # for file in os.listdirs()
+
+    # for file in 
+        # file = tar.extractfile(f"{faa_file}-structure_clusters.faa")
+        # content = file.read().decode()
+        # print(content)
+        # paths = tar.getnames()  # Lists all files inside
+    
+    # for file in paths:
+    #     if file.split(
+    # for file in os.listdir(f"data/{filename}"):
+    #     print(file)
+
