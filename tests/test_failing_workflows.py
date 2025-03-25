8907d6789d17053a61a1f9a546abb2ec87aa219b
Josh Bedwell <rcxwhiz@gmail.com>
2025-03-24 18:09:13 -0700

    Converted to new testing framework
    Still getting some very weird issues with the config changing itself and causing errors. Will try re-cloning the repo

--- tests/test_failing_workflows.py
+++ tests/test_failing_workflows.py
@@ -1,21 +1,24 @@
+import os
 import subprocess
 import unittest
 from pathlib import Path
 
+from unittest_scenarios import IsolatedWorkingDirMixin, ScenarioTestCaseMixin
+
 from tests.workflow_test_utils import copy_config, gather_files
-from tests.workflow_test_utils.scenario_testcase import ScenarioTestCase
 
 
-class TestFailingWorkflows(ScenarioTestCase, unittest.TestCase):
+class FailingWorkflowsTestCase(ScenarioTestCaseMixin, unittest.TestCase):
     scenarios_dir = Path(__file__).parent / "failing_scenarios"
-    check_strategy = ScenarioTestCase.OutputCheckOptions.NO_CHECK
-    link_items = {
-        Path(__file__).parent.parent / "utils": "link-make",
-        Path(__file__).parent.parent / "config": copy_config,
-        Path(__file__).parent.parent / "workflow": "link"
-    }
+    check_strategy = ScenarioTestCaseMixin.OutputChecking.NONE
+    external_connections = [
+        IsolatedWorkingDirMixin.ExternalConnection(external_path=str(Path(__file__).parent.parent / "utils")),
+        IsolatedWorkingDirMixin.ExternalConnection(external_path=str(Path(__file__).parent.parent / "workflow")),
+        IsolatedWorkingDirMixin.ExternalConnection(external_path=str(Path(__file__).parent.parent / "config"),
+                                                   strategy=copy_config)
+    ]
 
-    def run_scenario(self, scenario_name, expected_state_path):
-        expected_outputs = gather_files(expected_state_path)
-        result = subprocess.run(["snakemake", "--use-conda"] + expected_outputs)
+    def run_scenario(self, scenario_name: str, expected_state_path: str) -> None:
+        items_to_request = gather_files(os.path.join(expected_state_path, "final_state"))
+        result = subprocess.run(["snakemake", "--use-conda"] + list(items_to_request))
         self.assertNotEqual(0, result.returncode, "Snakemake completed normally")
--- tests/test_successful_workflows.py
+++ tests/test_successful_workflows.py
@@ -4,7 +4,6 @@ import unittest
 from pathlib import Path
 
 from tests.workflow_test_utils import copy_config, gather_files
-from tests.workflow_test_utils.scenario_testcase import ScenarioTestCase
 
 from unittest_scenarios import ScenarioTestCaseMixin, IsolatedWorkingDirMixin
 
@@ -13,35 +12,13 @@ class SuccessfulWorkflowsTestCase(ScenarioTestCaseMixin, unittest.TestCase):
     scenarios_dir = Path(__file__).parent / "successful_scenarios"
     external_connections = [
         IsolatedWorkingDirMixin.ExternalConnection(external_path=str(Path(__file__).parent.parent / "utils")),
-        IsolatedWorkingDirMixin.ExternalConnection(external_path=str(Path(__file__).parent.parent / "workflow"), strategy="copy"),
+        IsolatedWorkingDirMixin.ExternalConnection(external_path=str(Path(__file__).parent.parent / "workflow")),
         IsolatedWorkingDirMixin.ExternalConnection(external_path=str(Path(__file__).parent.parent / "config"), strategy=copy_config)
     ]
-    extra_final_items_allowed = True
+    match_final_state_exactly = False
 
 
     def run_scenario(self, scenario_name: str, scenario_path: str) -> None:
-        # if os.path.exists(os.path.join(scenario_path, "initial_state")):
-        #     input_items = gather_files(os.path.join(scenario_path, "initial_state"))
-        # else:
-        #     input_items = set()
-        # output_items = gather_files(os.path.join(scenario_path, "final_state"))
-        # items_to_request = output_items - input_items
-
         items_to_request = gather_files(os.path.join(scenario_path, "final_state"))
         result = subprocess.run(["snakemake", "--use-conda"] + list(items_to_request))
         self.assertEqual(0, result.returncode, f"Snakemake returned non-zero code: {result.returncode}")
-
-
-# class TestSuccessfulWorkflows(ScenarioTestCase, unittest.TestCase):
-#     scenarios_dir = Path(__file__).parent / "successful_scenarios"
-#     check_strategy = ScenarioTestCase.OutputCheckOptions.CHECK_FILE_CONTENTS
-#     link_items = {
-#         Path(__file__).parent.parent / "utils": "link-make",
-#         Path(__file__).parent.parent / "config": copy_config,
-#         Path(__file__).parent.parent / "workflow": "link"
-#     }
-#
-#     def run_scenario(self, scenario_name, expected_state_path):
-#         expected_outputs = gather_files(expected_state_path)
-#         result = subprocess.run(["snakemake", "--use-conda"] + expected_outputs)
-#         self.assertEqual(0, result.returncode, f"Snakemake returned non-zero code: {result.returncode}")
