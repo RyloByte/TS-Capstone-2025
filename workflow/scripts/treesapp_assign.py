67c225168e54b1d082f6c27ca8633c27a704dd6b
Thomas Janas <janast@berkeley.edu>
2025-04-13 02:11:42 +0000

    cleaned up script, but still not working

--- workflow/scripts/treesapp_assign.py
+++ workflow/scripts/treesapp_assign.py
@@ -4,8 +4,6 @@ import os
 import tempfile
 from tqdm import tqdm
 
-
-
 def run_treesapp_assign(input_fasta: str, output_dir: str, refpkg_path: str) -> None:
     os.makedirs(output_path, exist_ok=True)
     result = subprocess.run(
@@ -15,7 +13,7 @@ def run_treesapp_assign(input_fasta: str, output_dir: str, refpkg_path: str) -> 
              "-m", "prot", 
              "--trim_align", 
              "-o", output_dir,
-             "--refpkg", refpkg_path
+             "--refpkg_dir", refpkg_path
         ],
         capture_output=True,
         text=True
@@ -37,14 +35,12 @@ if __name__ == "__main__":
 
     # Outputs
     assigned_tar_output = snakemake.output[0]
-    # os.makedirs(assigned_hyperpackage, exist_ok=True)
 
     # Extract tar.gz into temp dir
     # with tempfile.TemporaryDirectory() as tmp:
     # tmp = tempfile.mkdtemp(prefix="treesapp_debug_")
     tmp = os.path.abspath("treesapp_debug")
     print(f"[DEBUG] Temporary working directory: {tmp}")
-    print(f"{tmp} this is the tmp pathhhh")
     with tarfile.open(hyperpackage, "r:gz") as tar:
         members = tar.getmembers()
         if len(members) == 0:
@@ -61,68 +57,8 @@ if __name__ == "__main__":
         if i >= 10:
             break
         refpkg_path = os.path.join(tmp, refpkg)
-        output_path = os.path.join(assigned_dir, refpkg)
+        output_path = os.path.join(assigned_dir, refpkg, "final_outputs.")
         if os.path.isdir(refpkg_path):
             run_treesapp_assign(fasta, refpkg_path, output_path)
 
     create_tar_gz(assigned_tar_output, assigned_dir)
-
-
-
-
-
-
-    # with tarfile.open(hyperpackage, "r:gz") as tar:
-        
-
-    #     with tempfile.TemporaryDirectory() as tmp_dir:
-            
-    #         tmp_pkg_path = os.path.join(tmp_dir, "assigned_packages")
-    #         os.makedirs(tmp_pkg_path, exist_ok=True)            
-
-    #         # Run treesapp assign on each reference package
-    #         for i, member in enumerate(tqdm(members, desc="Running treesapp assign", total=len(members))):
-    #             if i >= 10:
-    #                 break
-
-    #             # We only want to run on top-level .refpkg directories
-    #             top_level_dir = member.name.split("/")[0]
-    #             refpkg_path = os.path.join(tmp_dir, top_level_dir)
-
-    #             if not os.path.exists(refpkg_path):
-    #                 continue
-    #             output_path = os.path.join(tmp_pkg_path, top_level_dir)
-    #             os.makedirs(output_path, exist_ok=True)
-    #             run_treesapp_assign(fasta, output_path, refpkg_path)
-
-    #         create_tar_gz(assigned_hyperpackage, tmp_pkg_path)
-
-    #         print("Done")
-
-            # Bundle into assigned_hyperpackage
-            # curr_wd = os.getcwd()
-            # assigned_hyperpackage_dir = os.path.join(curr_wd, "data/assigned_hyperpackages")
-            # assigned_fasta_dir = os.path.join(assigned_hyperpackage_dir, "data/assigned_hyperpackages")
-            # os.makedirs(assigned_fasta_dir, exist_ok=True)
-
-
-# if __name__ == "__main__":
-#     #inputs
-#     hyperpackage = snakemake.input[0]
-#     fasta = snakemake.input[1]
-
-#     #outputs
-#     assigned_hyperpackage = snakemake.output[0]
-#     print(os.getcwd())
-
-#     with tarfile.open(hyperpackage, "r:gz") as tar:
-#         members = tar.getmembers()
-#         if len(members) == 0:
-#             raise RuntimeError(f"No reference packages found in {hyperpackage}")
-
-#         original_wd = os.getcwd()
-#         tmp_dir = original_wd
-
-#         for member in tqdm(members, desc="Running treesapp assign", total=len(members), unit="reference package"):
-#             run_treesapp_assign(fasta, )
-
