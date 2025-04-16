3f8931e7e474b38f03231bcc401b332deb86bc94
Thomas <janast@berkeley.edu>
2025-04-16 17:02:14 -0500

    Adding treesapp assign with tests

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -43,3 +43,34 @@ class RegressionTest(TestCase):
                 self.assertTrue(file_path.stat().st_size > 0, f"{file} is empty")
 
         os.chdir(original_wd)
+    
+    def test_assign_hyperpackage(self):
+        requested_files = [
+            "data/assigned_hyperpackages/rhea_10596.refpkg.tar.gz",
+        ]
+
+        original_wd = os.getcwd()
+        with tempfile.TemporaryDirectory() as tmpdir:
+            os.chdir(tmpdir)
+
+            os.symlink(project_dir / "workflow", "workflow")
+            os.symlink(project_dir / "utils", "utils")
+            os.symlink(tests_dir / "test_config.yaml", "config.yaml")
+
+            result = subprocess.run(
+                ["snakemake", "--use-conda", "--jobs", str(self.n_snakemake_cores)]
+                + requested_files
+            )
+
+            self.assertEqual(
+            
+                result.returncode,
+                f"Snakemake returned non-zero code: {result.returncode}",
+            )
+
+            for file in requested_files:
+                file_path = Path(file)
+                self.assertTrue(file_path.exists(), f"{file} was not created")
+                self.assertTrue(file_path.stat().st_size > 0, f"{file} is empty")
+
+        os.chdir(original_wd)
