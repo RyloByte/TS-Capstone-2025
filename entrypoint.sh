4a765b762f1bf7fe0dbb1147ac0a2a5af3c9b2dc
Josh Bedwell <rcxwhiz@gmail.com>
2025-04-23 15:26:16 -0700

    Finally fixed run script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-mkdir -p data utils results
-
 # check for/create user config file
 if [ ! -f "config.yaml" ]; then
   cp "config.yaml.example" "config.yaml"
--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ set -e
 
 IMAGE_NAME="ghcr.io/RyloByte/TS-Capstone-2025:latest"
 
+# make necessary directories
+mkdir -p data utils results
+
 # get number of processors
 if command -v nproc &> /dev/null; then
   NUM_PROCS=$(nproc)
@@ -12,8 +15,21 @@ else
 fi
 
 # run deleting container with mounts
-docker run \
-  --rm \
-  -v "$(pwd):/workflow-dir"
-  -it $IMAGE_NAME \
-  -j $NUM_PROCS "$@"
+if [ -f "config.yaml" ]; then
+  docker run \
+    --rm \
+    -v "$(pwd)/data:/workflow-dir/data" \
+    -v "$(pwd)/utils:/workflow-dir/utils" \
+    -v "$(pwd)/results:/workflow-dir/results" \
+    -v "$(pwd)/config.yaml:/workflow-dir/config.yaml" \
+    -it $IMAGE_NAME \
+    -j $NUM_PROCS "$@"
+else
+  docker run \
+    --rm \
+    -v "$(pwd)/data:/workflow-dir/data" \
+    -v "$(pwd)/utils:/workflow-dir/utils" \
+    -v "$(pwd)/results:/workflow-dir/results" \
+    -it $IMAGE_NAME \
+    -j $NUM_PROCS "$@"
+fi
